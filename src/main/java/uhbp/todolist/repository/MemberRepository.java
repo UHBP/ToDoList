0f7d16acad3f780662e3e4e1aa5773eb7cab8cde
J.M.Bae <bejm1997@gmail.com>
2023-08-16 22:52:17 +0900

    Create MemberRepository.java
    1️⃣ MemberRepo 인터페이스 추가

---
+++ src/main/java/uhbp/todolist/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package uhbp.todolist.repository;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uhbp.todolist.domain.Member;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}

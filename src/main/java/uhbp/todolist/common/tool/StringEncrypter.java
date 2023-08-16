cddde1603bcf58119b698df39de9a27d48444249
J.M.Bae <bejm1997@gmail.com>
2023-08-16 22:23:45 +0900

    Update StringEncrypter.java
    1️⃣ 해당 클래스 패키지 변경

--- src/main/java/uhbp/todolist/common/tool/StringEncrypter.java
+++ src/main/java/uhbp/todolist/common/tool/StringEncrypter.java
@@ -1,4 +1,4 @@
-package uhbp.todolist.common;
+package uhbp.todolist.common.tool;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -10,16 +10,23 @@ import java.security.NoSuchAlgorithmException;
 @Component
 public class StringEncrypter {
 
+    static final private String HASH_ALGORITHM = "SHA-256";
+
+    //TODO 구현용 임시 salt. 실제 서비스 시 감추어야 함
+    private String tempSalt = "salt";
+
     /**
      * 평문을 MD5로 해싱하여 변환
-     *
      * @param str
      * @return
      */
     public String doHash(String str) {
-        //TODO 암호화에 Salt 추가
+        // 받아온 String 에 salt 추가
+        str += tempSalt;
+
+        // 해시 알고리즘 시작
         try {
-            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            MessageDigest messageDigest = MessageDigest.getInstance(HASH_ALGORITHM);
             messageDigest.update(str.getBytes());
             byte[] digest = messageDigest.digest();
             StringBuilder encryptedStr = new StringBuilder();
@@ -29,7 +36,7 @@ public class StringEncrypter {
             return encryptedStr.toString();
         } catch (NoSuchAlgorithmException e) {
             log.info("암호화 실패", e);
-            return "";
+            throw new RuntimeException("암호화 실패");
         }
     }
 

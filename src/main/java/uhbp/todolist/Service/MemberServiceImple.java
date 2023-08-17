08493f8ddb283f9427e23da26e7a4e90677a5265
J.M.Bae <bejm1997@gmail.com>
2023-08-17 19:43:27 +0900

    Update MemberServiceImple.java
    1️⃣ 회원가입 Biz Logic 구현 완료

--- src/main/java/uhbp/todolist/Service/MemberServiceImple.java
+++ src/main/java/uhbp/todolist/Service/MemberServiceImple.java
@@ -1,11 +1,25 @@
 package uhbp.todolist.Service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uhbp.todolist.common.tool.StringEncrypter;
+import uhbp.todolist.domain.Member;
+import uhbp.todolist.dto.MemberJoinForm;
+import uhbp.todolist.repository.MemberRepository;
+
+import java.time.LocalDate;
 
 @Service
+@RequiredArgsConstructor
 public class MemberServiceImple implements MemberService {
+    
+    StringEncrypter encrypter;
+    MemberRepository memberRepository;
+    
     @Override
-    public void JoinMember() {
-
+    public void JoinMember(MemberJoinForm form) {
+        String encryptPw = form.getMemberPw();
+        Member member = Member.memberFactory(form.getMemberId(), encryptPw, form.getMemberNickName(), LocalDate.now());
+        Member savedMember = memberRepository.save(member);
     }
 }

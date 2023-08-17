441f124646cbc03fe24679bc8734761f89af044f
JMBAE <bejm1997@gmail.com>
2023-08-17 15:09:01 +0900

    Create TodoMemberManageTest.java
    1️⃣ TodoMemberManageFactory 테스트 케이스 Init

---
+++ src/test/java/uhbp/todolist/domain/TodoMemberManageTest.java
@@ -0,0 +1,12 @@
+package uhbp.todolist.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoMemberManageTest {
+
+    @Test
+    void todoMemberManageFactory() {
+    }
+}

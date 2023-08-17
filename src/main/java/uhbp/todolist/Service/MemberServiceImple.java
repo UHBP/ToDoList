58eced1dd8b9ea4c92b28010846588a8ff15fe8a
J.M.Bae <bejm1997@gmail.com>
2023-08-17 19:26:32 +0900

    Member 구현 클래스 어노테이션 추가
    1️⃣ 각 Bean 에 적합한 어노테이션 추가

--- src/main/java/uhbp/todolist/Service/MemberServiceImple.java
+++ src/main/java/uhbp/todolist/Service/MemberServiceImple.java
@@ -1,5 +1,11 @@
 package uhbp.todolist.Service;
 
+import org.springframework.stereotype.Service;
 
+@Service
 public class MemberServiceImple implements MemberService {
+    @Override
+    public void JoinMember() {
+
+    }
 }

3d713c2d306bc44fb9d4be308c41e5d12437c7de
J.M.Bae <bejm1997@gmail.com>
2023-08-19 15:04:23 +0900

    Update MemberLoginForm.java
    1. DTO 필드 및 메세지 수정

--- src/main/java/uhbp/todolist/dto/MemberLoginForm.java
+++ src/main/java/uhbp/todolist/dto/MemberLoginForm.java
@@ -3,15 +3,16 @@ package uhbp.todolist.dto;
 import com.sun.istack.NotNull;
 import lombok.*;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class MemberLoginForm {
-    @NotEmpty
+
+    @NotEmpty(message = "id 를 입력해주세요")
+    @Email(message = "id 는 이메일 형식입니다. ")
     private String inputId;
-    @NotEmpty
-    private String intpuPw;
+
+    @NotEmpty(message = "비밀번호를 입력해주세요")
+    private String inputPw;
 }

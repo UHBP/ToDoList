162b36104f23b691b285a86f73a64b0cb16e214e
JMBAE <bejm1997@gmail.com>
2023-08-31 15:37:21 +0900

    Create MemberServiceTest.java

---
+++ src/test/java/uhbp/todolist/Service/MemberServiceTest.java
@@ -0,0 +1,31 @@
+package uhbp.todolist.Service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@Transactional
+@SpringBootTest
+class MemberServiceTest {
+
+    @Test
+    void isMemberExist() {
+    }
+
+    @Test
+    void joinMember() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void isDuplicateMemberId() {
+    }
+}

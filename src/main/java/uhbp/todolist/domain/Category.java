91e54cbf15ab81049cccd68321b96b4fe11e315b
J.M.Bae <bejm1997@gmail.com>
2023-09-07 18:32:05 +0900

    Update Category.java
    1. Front 에서 넘어온 Index 를 활용하여 Enum 생성

--- src/main/java/uhbp/todolist/domain/Category.java
+++ src/main/java/uhbp/todolist/domain/Category.java
@@ -6,6 +6,16 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Category {
-    STUDY("공부"), EXERCISE("운동"), APPOINTMENT("약속"), OTHER("기타");
-    private String value;
+    STUDY(1), EXERCISE(2), APPOINTMENT(3), OTHER(4);
+    private int index;
+
+    // index를 기반으로 Enum을 생성하는 정적 메서드
+    public static Category fromIndex(int index) {
+        for (Category enumValue : values()) {
+            if (enumValue.getIndex() == index) {
+                return enumValue;
+            }
+        }
+        throw new IllegalArgumentException("해당 인덱스를 가진 Enum 상수가 없습니다.");
+    }
 }

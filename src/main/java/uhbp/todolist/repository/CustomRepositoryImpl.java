cb18b442ae0ff244a95656c681bf047ce283876a
Park Yoon A <pya7106@naver.com>
2023-09-07 14:17:30 +0900

    QueryDSL 적용
    1️⃣ build.gradle 에 QueryDSL 관련한 내용 작성함
    2️⃣ CustomRepositoryImpl QueryDSL 적용하여 수정함
        - EntityManager 대신 JPAQueryFactory 적용
        - QueryDSL 을 통해 생성된 QMember 클래스로 조회

--- src/main/java/uhbp/todolist/repository/CustomRepositoryImpl.java
+++ src/main/java/uhbp/todolist/repository/CustomRepositoryImpl.java
@@ -1,5 +1,6 @@
 package uhbp.todolist.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import uhbp.todolist.domain.Member;
@@ -8,15 +9,29 @@ import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import java.util.List;
 
+import static uhbp.todolist.domain.QMember.member;
+
 @Repository
 @Transactional
 public class CustomRepositoryImpl implements CustomRepository {
     @Autowired
-    EntityManager em;
+    private EntityManager em;
+
+    /**
+     * - EntityManager 대신 JPAQueryFactory 적용
+     * - QueryDSL 을 통해 생성된 QMember 클래스로 사용자 조회
+     * - QMember 클래스 : Member Entity 를 상속
+     * @param searchId 검색할 사용자의 아이디 일부
+     * @return
+     */
     @Override
     public List<Member> findMember(String searchId) {
-        return em.createQuery("SELECT m FROM Member m WHERE m.memberId LIKE :searchId", Member.class)
-                .setParameter("searchId", "%"+searchId+"%")
-                .getResultList();
+        // JPAQueryFactory : JPAQuery를 생성해주는 factory 클래스
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+
+        return queryFactory
+                .selectFrom(member)
+                .where(member.memberId.like("%" + searchId + "%"))
+                .fetch();  // fetch() : 조회 대상 전체를 반환
     }
 }

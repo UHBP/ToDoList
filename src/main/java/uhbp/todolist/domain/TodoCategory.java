6979babb9ca176ef7d3cd6a48bafba3d55289810
JMBAE <bejm1997@gmail.com>
2023-09-08 13:30:44 +0900

    ApplyEnum

--- src/main/java/uhbp/todolist/domain/TodoCategory.java
+++ src/main/java/uhbp/todolist/domain/TodoCategory.java
@@ -6,21 +6,5 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum TodoCategory {
-    STUDY(1), EXERCISE(2), APPOINTMENT(3), OTHER(4);
-    private int categoryIndex;
-
-    public int getIndex() {
-        return categoryIndex;
-    }
-
-    // index를 기반으로 Enum을 생성하는 정적 메서드
-    public static TodoCategory fromCategoryIndex(int categoryIndex) {
-        for (TodoCategory enumValue : values()) {
-            if (enumValue.getCategoryIndex() == categoryIndex) {
-                return enumValue;
-            }
-        }
-        throw new IllegalArgumentException("해당 인덱스를 가진 Enum 상수가 없습니다.");
-    }
-
+    STUDY, EXERCISE, APPOINTMENT, OTHER;
 }

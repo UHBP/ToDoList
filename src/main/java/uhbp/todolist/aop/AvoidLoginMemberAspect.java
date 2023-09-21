9221b459075a94b2d20b8d55a1591b7a81e48c04
J.M.Bae <78770745+Dalaranian@users.noreply.github.com>
2023-09-21 23:44:43 +0900

    Jung min aop#1 (#45)
    * 횡단 공통 관심사 분리
    
    1. 로그인된 유저가 login / join 페이지 접근 거부 관심사 AOP 로 분리
    
    * Runtime Error Message Handling
    
    * Errorpage 수정
    
    500 Error 일 시, Exception 에 Message 전시되게 수정
    
    * Update AvoidLoginMemberAspect.java
    
    1. Exception message 변경

---
+++ src/main/java/uhbp/todolist/aop/AvoidLoginMemberAspect.java
@@ -0,0 +1,29 @@
+package uhbp.todolist.aop;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import uhbp.todolist.session.CookieMemberStore;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@Aspect
+@Slf4j
+@RequiredArgsConstructor
+public class AvoidLoginMemberAspect {
+
+    private final CookieMemberStore cookieMemberStore;
+
+    @Before("execution(* uhbp.todolist.Controller.*.login(..)) && args(model,request,..) || " +
+            "execution(* uhbp.todolist.Controller.*.join(..)) && args(model,request,..)")
+    public void checkCookie(JoinPoint joinPoint, Model model, HttpServletRequest request){
+        if(cookieMemberStore.findValueByKey(request) != null){
+            throw new IllegalStateException("로그인된 사용자에게 허가되지 않은 기능입니다. ");
+        }
+    }
+}

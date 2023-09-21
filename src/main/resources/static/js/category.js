5424476b4814521c1e5b56d23c4e86b27c82b364
Yunji97 <simyg97@naver.com>
2023-09-21 20:02:45 +0900

    Create sort.js&category.js

---
+++ src/main/resources/static/js/category.js
@@ -0,0 +1,16 @@
+    function selectCategory(category) {
+    // 선택한 카테고리 값을 서버로 전송
+    $.ajax({
+        type: 'GET',
+        url: '/todo/selectCategory',
+        data: { category: category },
+        success: function (data) {
+            // 성공적으로 데이터를 받았을 때
+            $('#todoListContainer').html(data);
+            console.log('카테고리 필터링 성공');
+        },
+        error: function () {
+            console.error('카테고리 필터링 실패');
+        }
+    });
+}
